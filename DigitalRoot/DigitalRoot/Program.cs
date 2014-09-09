62ae474e8b6d37ffe8dfade14cefbb1f87788690
jaya chunduri <jaya.chunduri@gmail.com>
2014-09-08 22:00:26 -0600

    DigitalRoot

---
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digital Root of 31337 is: ");
+            DigitalRoot(31337);
+
+            Console.WriteLine("\nDigital Root of 45734 is: ");
+            DigitalRoot(45734);
+        }
+
+        static void DigitalRoot(int num)
+        {
+            string sNum = num.ToString();
+            int total = 0; //variable to keep track of total of the numbers
+
+            //looping through each number
+            foreach (var item in sNum)
+            {
+                total += int.Parse(item.ToString());
+            }
+
+            if (total.ToString().Length > 1) //means total is more than one digit
+            {
+                DigitalRoot(total);
+            }
+
+            else
+                Console.WriteLine(total);
+        }
+    }
+}
